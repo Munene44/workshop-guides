5ac97a4cc8c1dcbb4673b87e3d7c6a0b0544d6fc
Munene44 <karvinmitchar@gmail.com>
2023-03-29 10:46:30 +0300

    add comments route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/about'
   root "pages#homepage"   
-  resources :ideas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+resources :ideas do
+  resources :comments
+end
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
